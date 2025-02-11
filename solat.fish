9617bbf082f50574764d720990964edc89dc73b4
Muhammad Noh <54850652+madnoh@users.noreply.github.com>
2025-02-11 11:19:31 +0800

    Update solat.fish
    Set location code

--- solat.fish
+++ solat.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
-#
+
+# Set location code
+set location jhr-2
 
 # Get the current date in DD-MM-YYYY format
 set today (date "+%d-%m-%Y")
@@ -13,14 +15,14 @@ set hijri_month (echo "$response" | jq -r '.data.hijri.month.en')
 set hijri_year (echo "$response" | jq -r '.data.hijri.year')
 
 # Print the results
-echo -n "Solat times for today $hijri_date $hijri_month $hijri_year or "
+echo -n "$location solat times for today $hijri_date $hijri_month $hijri_year or "
 date "+%d %B %Y"
 
 # Define prayer names
 set prayers "Fajr" "Shurooq" "Dhuhr" "Asr" "Maghrib" "Isha"
 
 # Fetch the JSON data and process it silently
-set times (curl -s "https://mpt.i906.my/mpt.json?code=jhr-2&filter=1" | jq -r '.response.times[] | (.+28800) | strftime("%H:%M")')
+set times (curl -s "https://mpt.i906.my/mpt.json?code=$location&filter=1" | jq -r '.response.times[] | (.+28800) | strftime("%H:%M")')
 
 # Iterate over the times and prefix them with the corresponding prayer name
 for i in (seq (count $times))
